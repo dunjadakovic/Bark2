3c29a5bc24ee6b5b5bed81eb2d8f44156b504784
dunjadakovic <129779699+dunjadakovic@users.noreply.github.com>
2024-01-08 20:50:35 +0100

    Update model.py

--- bark/model.py
+++ bark/model.py
@@ -86,6 +86,8 @@ class CausalSelfAttention(nn.Module):
             att = self.attn_dropout(att)
             y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
         y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+        print(q)
+        print(k)
 
         # output projection
         y = self.resid_dropout(self.c_proj(y))
